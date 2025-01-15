9b751cf2a278ffd91f858059452e971be10cec2d
terry <terry.cadd@gmail.com>
2025-01-15 17:56:18 +0000

    plodding on

--- day8/common-lisp/fun.lisp
+++ day8/common-lisp/fun.lisp
@@ -10,6 +10,20 @@
 (defparameter file (let ((xs (uiop:read-file-lines "../input.txt")))
                      (make-array (length xs) :initial-contents xs)))
 
+(setq file #("............"
+             "........0..."
+             ".....0......"
+             ".......0...."
+             "....0......."
+             "......A....."
+             "............"
+             "............"
+             "........A..."
+             ".........A.."
+             "............"
+             "............"))
+
+
 (defparameter wid (length (aref file 0)))
 (defparameter hgt (length file))
 
@@ -61,22 +75,76 @@
 
 can we visually check this 
 
-
 |#
 
 ;; given all locations of letter h 
 ;; iterate through list making pairs 
 (defun pair-locations (xs)
-  (let ((i 0)(j 0)(lim (length xs))(pairs '()))
+  (let ((lim (length xs))
+        (nodes '()))
     (loop for i from 0 to (- lim 1) do
       (let ((a (nth i xs)))
-        (loop for j from 1 to (- lim 1) do
+        (loop for j from (+ i 1) to (- lim 1) do
           (let ((b (nth j xs)))
             (let ((ax (car a))(ay (car (cdr a)))
                   (bx (car b))(by (car (cdr b))))
-              (let ((dx (- ax bx))
-                    (dy (- ay by)))
+              (let ((dx (- bx ax))
+                    (dy (- by ay)))
+                (let* ((anti-x (- ax dx))
+                       (anti-y (- ay dy))
+                       (pos (list anti-x anti-y)))
+                  (when (and (on-map anti-x anti-y) (not(member pos nodes :test 'equalp)))
+                    (format t "~a ~a ~a ~a -> ~a ~a -> ~a ~a~%" ax ay bx by dx dy anti-x anti-y))
+                    (setq nodes (cons (list anti-x anti-y) nodes)))
+
+                (let* ((anti-x (+ bx dx))
+                       (anti-y (+ by dy))
+                       (pos (list anti-x anti-y)))
+                  (when (and (on-map anti-x anti-y) (not(member pos nodes :test 'equalp)))
+                    (format t "~a ~a ~a ~a -> ~a ~a -> ~a ~a ~%" ax ay bx by dx dy anti-x anti-y))
+                    (setq nodes (cons (list anti-x anti-y) nodes)))
+                  
+                ))))))
+    nodes))
+
+(defun find-anti-nodes ()
+  (let ((letters (find-letters))
+        (all-nodes '()))
+    (dolist (letter letters)
+      (let* ((locations (find-locations letter))
+             (nodes (pair-locations locations)))
+        (dolist (node nodes)
+          (when (not(member node all-nodes :test 'equalp))
+            (setq all-nodes (cons node all-nodes))))))
+    (format t "there are ~a unique antinodes ~%" (length all-nodes))
+    (show all-nodes)))
+
+(defun show (nodes)
+  (loop for y from 0 to (- hgt 1) do
+    (format t "~%")
+    (loop for x from 0 to (- wid 1) do
+      (let ((pos (list x y)))
+        (cond
+          ((member pos nodes :test 'equalp) (format t "#"))
+          (t (format t "."))))))
+    (format t "~%"))
+
+
+  
+
+
+
+
+        
+        
+        
+
+
+
+
+
                 
+                  
                   
               
             
