000fd4cc2562c23b895c2332986cdb298aa577f7
terry <terry.cadd@gmail.com>
2025-03-21 11:29:00 +0000

    progess ! solved part 1 of day 16

--- day16/guile/breadth2.scm
+++ day16/guile/breadth2.scm
@@ -149,6 +149,33 @@ the todo list will then have a set of squares that have potential to be optimal
     (let ((g (car (cdr (assoc 'grid ty)))))
       (vector-set! (vector-ref g y) x z))))
 
+;; how define it ? how does show-grid do it ?
+(define (in-grid? g x y)
+  (let ((wid (second (assoc 'wid g)))
+	(hgt (second (assoc 'hgt g))))
+    (and (>= x 0)(>= y 0)(< x wid)(< y hgt))))
+
+;; at-grid :: total
+;; either returns false meaning you are off the grid itself
+;; OR returns what should be at the grid
+;; doesnt type check but could massage it to a type
+(define (at-grid g x y)
+  (let ((wid (second (assoc 'wid g)))
+	(hgt (second (assoc 'hgt g))))
+    (if (and (>= x 0)(>= y 0)(< x wid)(< y hgt))
+	(internal-xy g x y)
+	#f)))
+   
+(define (width-of g)
+  (second (assoc 'wid g)))
+
+(define (height-of g)
+  (second (assoc 'hgt g)))
+
+;; zero based indexes ??
+
+  
+
 ;; given a grid , find the robot ... loops past even tho found it
 ;; not important
 
@@ -176,6 +203,7 @@ the todo list will then have a set of squares that have potential to be optimal
 	  ;;(fmt #t "caught exception ~a ~%" e)
 	  location)))))
 
+
 ;; ;; shows grid g 
 (define (show-grid g)
   (let ((wid (second (assoc 'wid g)))
@@ -191,14 +219,17 @@ the todo list will then have a set of squares that have potential to be optimal
 	    ;;(fmt #t "checking location ~a ~a ~%" x y)
 	    (let ((ch (internal-xy g x y)))
 	      (cond
+	       ((not (char? ch)) (fmt #t "(~a)" ch))
 	       ((char=? ch #\S) (fmt #t "~a" #\S))
 	       ((char=? ch #\E) (fmt #t "~a" #\E))	
 	       ((char=? ch #\#) (fmt #t "~a" #\#))       
 	       ((char=? ch #\.) (fmt #t "~a" #\.))
-	       (#t (fmt #t "?~a?" ch))))
+	       (#t (fmt #t "[~a]" ch))))
 	    (loop-x (+ x 1)))))
 	(loop-y (+ y 1)))))))
 
+
+;; ===================================================================================
 
 
 (define make-four-vector
@@ -253,39 +284,6 @@ the todo list will then have a set of squares that have potential to be optimal
 	(vector-ref xvec di)))))
 
 
-;; ============== RUNTIME ===================================
-;; read in grid from a file
-;; figure out start and end positions
-;; erase those from the grid
-;; grid only has either # meaning not accessible
-;; or . meaning available to move through
-
-(define g (to-array (read-example "../example1.txt")))
-;;(define g (to-array (read-example "../input.txt")))
-(define start (find-in-grid g (lambda (ch) (char=? ch #\S))))
-(define end (find-in-grid g (lambda (ch) (char=? ch #\E))))
-
-(define start-x (car start))
-(define start-y (car (cdr start)))
-
-(define end-x (car end))
-(define end-y (car (cdr end)))
-
-;; erase S in grid 
-;; erase E in grid
-(let* ((xy start)(x (car xy))(y (car(cdr xy))))
-  (internal-xy! g x y #\.))
-(let* ((xy end)(x (car xy))(y (car(cdr xy))))
-  (internal-xy! g x y #\.))
-
-(set! wid (second (assoc 'wid g)))
-(set! hgt (second (assoc 'hgt g)))
-
-;; score doesnt need to be reset
-(define score (make-score-vector wid hgt))
-;; 
-
-
 ;; ======================================================
 
 
@@ -322,265 +320,174 @@ the todo list will then have a set of squares that have potential to be optimal
 (define best -1)   
 
 
-;; do breadth first search 
-(define search
-  (lambda ()
-    (letrec
-	((hunt (lambda (x y dir cost)		 
-		 
-		 (cond
-		  ((char=? (internal-xy g x y) #\#) #f) ;; wall
-		  ((and (= x end-x) (= y end-y))
-		   (fmt #t " found target at ~a ~a with cost of ~a ~%" x y cost)
-		   (when (or (< cost best)(= best -1))
-		     (set! best cost))
-		   #t)
-		  (#t ;; not a wall
-		   ;;(fmt #t "at ~a ~a " x y)
-		   (let ((s (score-get score x y dir)))      
-		     ;; record reached a new location 
-		     ;;(fmt #t " : score ~a " s)
-		     
-		     (when (< s 0)
-		       (score-set! score x y dir cost))
-		     ;; if the cost to reach this square is higher than that already found then discontinue search
-		     (cond
-		      ((< (score-get score x y dir) cost)
-		       ;;(fmt #t " : aborted ~a ~%" cost)
-		       #f)
-)
-		      (#t
-		       ;; forward
-		       ;;(fmt #t "forward ! ~%")
+;; ============== RUNTIME ===================================
+;; read in grid from a file
+;; figure out start and end positions
+;; erase those from the grid
+;; grid only has either # meaning not accessible
+;; or . meaning available to move through
 
-		       (cond
-			((eq? dir 'east) (hunt (+ x 1) y dir (+ cost 1)))
-			((eq? dir 'west) (hunt (- x 1) y dir (+ cost 1)))
-			((eq? dir 'south) (hunt x (+ y 1) dir (+ cost 1)))
-			((eq? dir 'north) (hunt x (- y 1) dir (+ cost 1)))
-			(#t (error "hunt bad dir")))
-		       
-		       ;; turn left
-		       ;;(fmt #t "left ! ~%")
-		       (hunt x y (clockwise dir) (+ cost 1000))
-		       
-		       ;; turn right
-		       ;;(fmt #t "right ! ~%")
-		       (hunt x y (anticlockwise dir) (+ cost 1000))
-		       )))))))
-      
-      (hunt start-x start-y 'east 0))))
+;;(define g (to-array (read-example "../example1.txt")))
+;;(define g (to-array (read-example "../example2.txt")))
+(define g (to-array (read-example "../input.txt")))
 
+(define start (find-in-grid g (lambda (ch) (char=? ch #\S))))
+(define end (find-in-grid g (lambda (ch) (char=? ch #\E))))
 
+(define start-x (car start))
+(define start-y (car (cdr start)))
 
+(define end-x (car end))
+(define end-y (car (cdr end)))
 
-;; (define example1 (read-example "../example1.txt"))
-;; (define example2 (read-example "../example2.txt"))
-;; (define input1 (read-example "../input.txt"))
+;; erase S in grid 
+;; erase E in grid
+(let* ((xy start)(x (car xy))(y (car(cdr xy))))
+  (internal-xy! g x y #\.))
+(let* ((xy end)(x (car xy))(y (car(cdr xy))))
+  (internal-xy! g x y #\.))
 
+(set! wid (second (assoc 'wid g)))
+(set! hgt (second (assoc 'hgt g)))
 
-;; ;; the grid
-;; (define problem #f)
-;; (define g #f)
-;; ;; robot position 
-;; (define r #f)
-;; ;; (fmt #t "robot is at ~a ~%" r)
-;; (define rx #f)
-;; (define ry #f)
-;; (define wid #f)
-;; (define hgt #f)
-;; (define wid-1 #f)
-;; (define hgt-1 #f) 
-;; (define moves #f)
+;; score doesnt need to be reset
+;;(define score (make-score-vector wid hgt))
+;;
+
+;; (anticlockwise 'east)
+;; (anticlockwise 'north)
+;; (anticlockwise 'west)
+;; (anticlockwise 'south)
+
+;; state s = x y dir cost
+
+#|
+turning left or right simply updates new direction and adds 1000 onto current cost
+|#
+(define (state-left s)
+  (let ((x (list-ref s 0))
+	(y (list-ref s 1))
+	(dir (list-ref s 2))
+	(cost (list-ref s 3)))	
+    (list x y (anticlockwise dir) (+ cost 1000))))
 
 
 
-
-;; ;; g grid
-;; ;; m move we have in mind left right up down
-;; ;; we build up a stack of procedures which if eventually find we can complete all the moves
-;; ;; all get executed , such that last robot moves first .. first robot moves last .. nice outcome
-;; ;; albeit side effect full mutation 
-;; (define (step-left)
-;;   (step-generic (lambda (x) (- x 1)) (lambda (y) y)))
-
-;; (define (step-right)
-;;   (step-generic (lambda (x) (+ x 1)) (lambda (y) y)))
-
-;; (define (step-down)
-;;   (step-generic (lambda (x) x) (lambda (y) (+ y 1))))
-
-;; (define (step-up)
-;;   (step-generic (lambda (x) x) (lambda (y) (- y 1))))
-
-
-;; (define (step-generic fx fy)
-;;   (let* ((stack '()))
-;;     (letrec ((loop (lambda (x y)
-;; 		     (cond
-;; 		      ;; cannot move a wall - unwind without executing stack		       
-;; 		      ((and (<= x 0)(>= x wid-1) (<= y 0) (>= y hgt-1)) #f)
-		       
-;; 		      (#t 
-;; 		       (let ((ch (internal-xy g x y)))
-;; 			 ;;(fmt #t "step: looking at position ~a ~a  found char ~a ~%" x y ch)
-;; 			 (cond
-			  
-;; 			   ;; cannot move a wall - unwind without executing stack
-;; 			  ((char=? ch #\#) #f)
-			  
-;; 			   ;; can move object O 
-;; 			  ((char=? ch #\O)
-;; 			   (set! stack (cons (lambda ()
-;; 					       (internal-xy! g x y #\.) ;; clear space occupied by object
-;; 					       (internal-xy! g (fx x) (fy y) #\O)) ;; object is now one position left
-;; 					     stack))
-;; 			   ;; look at object to left
-;; 			   (loop (fx x) (fy y)))
-			  
-;; 			  ;; empty space - activate the stack unwind			  
-;; 			  ((char=? ch #\.) (execute-stack stack) #t)
-;; 			  (#t
-;; 			   (fmt #t "unrecognised char ~a in grid at ~a ~a ~%" ch x y)
-;; 			   (error "step"))))))))
-;; 	     (execute-stack (lambda (xs)
-;; 			      (cond
-;; 			       ((null? xs) xs)
-;; 			       (#t (let ((fn (car xs)))
-;; 				     (fn)
-;; 				     (execute-stack (cdr xs))))))))
-;;       ;; debug
-;;       ;;(fmt #t "step : robot is at ~a ~a ~%" rx ry)
-;;       ;; robot will always be able to move left
-;;       ;; add that move to the stack
-;;       (set! stack (cons (lambda ()
-;; 			  (internal-xy! g rx ry #\.) ;; clear space occupied by robot
-;; 			  (internal-xy! g (fx rx) (fy ry) #\@) ;; robot is now one position left
-;; 			  ;;(fmt #t "changing robot position from ~a ~a to " rx ry)
-;; 			  (set! rx (fx rx)) ;; change global position of robot
-;; 			  (set! ry (fy ry))
-;; 			  ;;(fmt #t " =>  ~a ~a ~%" rx ry)
-
-;; 			)
-;; 			stack))
-;;       (loop (fx rx) (fy ry)))))
-
-
-;; (define (score-grid)
-;;   (let ((score 0))
-;;     (letrec ((foo (lambda (x y)
-;; 		    (cond
-;; 		     ((>= y hgt) score)
-;; 		     ((>= x wid) (foo 0 (+ y 1)))
-;; 		     (#t (let ((ch (internal-xy g x y)))
-;; 			   (cond
-;; 			    ((char=? ch #\O)
-;; 			     (set! score (+ score (* 100 y) x))))
-;; 			   (foo (+ x 1) y)))))))
-;;       (foo 0 0))))
+(define (state-right s)
+  (let ((x (list-ref s 0))
+	(y (list-ref s 1))
+	(dir (list-ref s 2))
+	(cost (list-ref s 3)))
+    (list x y (clockwise dir) (+ cost 1000))))
 
 
 
+#|
+going forward simply increase cost by 1 in direction dir suggests
+we make no attempt to determine from a grand high level overview if such a state change
+is in any way legal or illegal
+|#
+(define (state-forward s)
+  (let ((x (list-ref s 0))
+	(y (list-ref s 1))
+	(dir (list-ref s 2))
+	(cost (list-ref s 3)))
+    (cond
+     ((eq? dir 'north) (list x (- y 1) dir (+ cost 1)))
+     ((eq? dir 'east)  (list (+ x 1) y dir (+ cost 1)))
+     ((eq? dir 'south) (list x (+ y 1) dir (+ cost 1)))
+     ((eq? dir 'west)  (list (- x 1) y dir (+ cost 1)))
+     (#t (error (format #f "bad direction state foward on ~a " s))))))
 
 
 
+#|
+determinining if a state is legal
+ depends on the grid we are working with 
+ is it inside the boundary of grid
+ is what is at the grid x y an empty square '.'
+|#
+(define (legal? g s)
+  (let ((x (car s))
+	(y (cadr s))
+	(dir (caddr s))
+	(cost (cadddr s)))
+    (equal? (at-grid g x y) #\.)))
+
+;; worthy of further consideration
+;; hash table with (x y dir) as key
+;; value is the cost
+;; if no entry in hash table then yes should be considered further as lead to new routes
+;; if cost value in hash is lower than cost value in state - do not consider further
+;; if cost value in hash is equal to cost value in state - do not consider further
+;; if cost value in hash is greater than cost value in state - found a better route
+;;  definitely consider further
 
 
-;; (define (reset)
-;;   (set! example1 (read-example "../example1.txt"))
-;;   (set! example2 (read-example "../example2.txt"))
-;;   (set! input1 (read-example "../input.txt"))
-;;   ;; choose your weapon 
-;;   (set! problem input1)
-;;   (set! g (to-array (find-grid problem)))
-;;   (set! r (find-robot g))
-;;   ;; (fmt #t "robot is at ~a ~%" r)
-;;   (set! rx (car r))
-;;   (set! ry (car (cdr r)))
-;;   (set! wid (second (assoc 'wid g)))
-;;   (set! hgt (second (assoc 'hgt g)))
-;;   (set! wid-1 (- wid 1))
-;;   (set! hgt-1 (- hgt 1))
-;; ;;  (set! moves (find-moves problem))
-;; )  
-
-
-;; (define (run)
-;;   (reset)
-;;   (fmt #t "running ........~%")
-;;   ;; know where robot is at all times using rx ry
-;;   (let ((count -1))
-;;     (letrec ((next (lambda (xs)
-;; 		     (set! count (+ count 1))
-;; 		     ;;(fmt #t "count [~a] ~%" count)
-;; 		     ;;(show-grid g)
-;; 		     (cond
-;; 		      ((null? xs) #t)
-;; 		      (#t (let ((ch (car xs)))			    
-;; 			    ;;(fmt #t " : move [~a] ~%" ch)			    
-;; 			    (cond
-;; 			     ((char=? ch #\<) (step-left))
-;; 			     ((char=? ch #\>) (step-right))
-;; 			     ((char=? ch #\^) (step-up))
-;; 			     ((char=? ch #\v) (step-down))
-;; 			     (#t (fmt #t "run next ch ~a : error bad char~%" ch)
-;; 				 (error "next")))
-;; 			    (next (cdr xs))))))))
-;;       (next moves)
-;;       (let ((result (score-grid)))
-;; 	(fmt #t "score = ~a ~%" result)
-;; 	result))))
-
-
-
-
-
-	       
-
-
-;; #|
-;; think array may not be best approach here
-
-;; (define g (to-array (find-grid example2)))
-;; (internal-xy g 2 2)
-;; (internal-xy g 2 4)
-;; (internal-xy g 3 1)
-;; ;;(internal-xy! g 3 1 #\t)
-;; (pp g )
-;; ;;(internal-xy! g 0 0 #\s)
-;; (pp g )
-;; (define gm (find-moves example2))
-
-;; #\# is a wall character
-;; #\. is empty square
-;; #\O is something to move like a box
-;; #\@ is the robot
-
-;; we always start with robot x , y 
-;; look at move  direction < left   right >   ^up  down v
-;; depednign on the direction we look to see what implications of making the move
-
-;; robot to go left
-;; robot next position is (x-1,y)
-;; look at (x-1,y) and ask is there push back - ie refusal to move ?
-;; if #\# wall character then there is pushback , simply move onto next move , nobody moves
-;; if #\. empty no pushback , everything mentioned up till now gets to move
-;; if O object , then it will also ask neighbour in direction of move if that too can move ?
-;; builds up a stack , if at end we find we can make the moves , then the stack is unwound with moves applied
-;; and when stack is empty , next move can be applied
-
-
-;; |#
+;; FURTHER : global hash table takes in state and make recommendation to keep state
+;; LOOKUP :  given x y coordinates shows the minimal states that reached here
+(define further? #f)
+(define lookup #f)
+(let ((h (make-hash-table)))
+  (letrec ((foo (lambda (s)		  
+		  (let ((x (car s))
+			(y (cadr s))
+			(dir (caddr s))
+			(cost (cadddr s)))
+		    (let* ((key (list x y dir))
+			   (value (hash-ref h key 'no-entry)))
+		      (cond
+		       ((eq? value 'no-entry) (hash-set! h key cost) #t)
+		       ((<= value cost) #f)
+		       (#t (hash-set! h key cost) #t))))))
+	   (bar (lambda (x y)
+		  (list (hash-ref h (list x y 'north) 'no-entry)
+			(hash-ref h (list x y 'east) 'no-entry)
+			(hash-ref h (list x y 'south) 'no-entry)
+			(hash-ref h (list x y 'west) 'no-entry)))))
+    (set! further? foo)
+    (set! lookup bar)))
 
 
 
+(define zero-cost 0)
+(define empty-path '())
+(define work-set (list (list start-x start-y 'east zero-cost)))
+(define todo-set '())
+
+(define (find)
+  (cond
+   ((null? work-set)
+    (set! work-set todo-set)
+    (set! todo-set '())
+    (cond
+     ((null? work-set) 'done-do-lookup-on-end-x-end-y)
+     (#t (find))))
+   (#t (let ((s (car work-set)))
+	 (set! work-set (cdr work-set))
+	 (let ((sleft (state-left s))
+	       (sright (state-right s))
+	       (sforward (state-forward s)))
+	   (letrec ((consider! (lambda (x)
+				 (when (and (legal? g x) (further? x))
+				   (set! todo-set (cons x todo-set))))))
+	     (consider! s)
+	     (consider! sleft)
+	     (consider! sright)
+	     (consider! sforward)
+	     (find)))))))
 
 
+(define (part-1)
+  (find)
+  (sort (lookup end-x end-y) <))
 
+#|
+scheme@(guile-user)> (part-1)
+$90 = (133584 134584 134584 135584)
+        ^^^--- best score
 
+133584 accepted answer
 
-
-
-
-
+|#
 
